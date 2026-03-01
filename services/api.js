12d0a42901d08e718c10f54f13f32c473e0b2a1e
Ayoub Jdad <ayoubjdad1@gmail.com>
2026-03-01 16:01:47 +0000

    v1

--- services/api.js
+++ services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as Storage from "../store/storage";
 
 export const api = axios.create({
-  baseURL: "https://rpi-jyqt.onrender.com/api",
+  baseURL: "https://sahbi-backend.onrender.com/",
   // baseURL: "http://localhost:5000/api",
 });
 
