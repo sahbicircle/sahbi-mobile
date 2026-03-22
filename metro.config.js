b3e6464af5f306e1d58d1b65e05078956152ff21
Ayoub Jdad <ayoub.jdad@weexa.com>
2026-03-23 00:13:36 +0100

    v1.2.2 build

--- metro.config.js
+++ metro.config.js
@@ -1,8 +1,15 @@
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
-config.transformer.babelTransformerPath =
-  require.resolve("react-native-sass-transformer");
-config.resolver.sourceExts.push("scss");
+
+// Add SASS/SCSS support for React Native (Expo's default only supports CSS on web)
+// Use spread to preserve _expoRelativeProjectRoot and other Expo transformer props (required for EAS Build)
+config.transformer = {
+  ...config.transformer,
+  babelTransformerPath: require.resolve("react-native-sass-transformer"),
+};
+if (!config.resolver.sourceExts.includes("scss")) {
+  config.resolver.sourceExts.push("scss", "sass");
+}
 
 module.exports = config;
